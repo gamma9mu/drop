7e551d8c306bacd5e5111be61e38c999c3d64772
Brian Guthrie <baguthrie@sbcglobal.net>
2013-05-12 18:45:55 -0500

    Add database interface.

---
+++ db.h
@@ -0,0 +1,41 @@
+#ifndef DB_H__
+#define DB_H__
+
+typedef bool  (*close_func)(void*);
+typedef void *(*create_cursor_func)(void*);
+typedef bool  (*cursor_first_func)(void*, void*);
+typedef bool  (*cursor_next_func)(void*, void*);
+typedef char *(*cursor_key_func)(void*, void*);
+typedef char *(*cursor_value_func)(void*, void*);
+typedef bool  (*delete_func)(void*, const char*);
+typedef void  (*destroy_cursor_func)(void*);
+typedef int   (*errno_func)(void*);
+typedef char *(*fetch_func)(void*, const char*);
+typedef void *(*open_func)(const char*);
+typedef bool  (*store_func)(void*, char*, char*);
+typedef const char *(*strerror_func)(int);
+typedef bool  (*try_store_func)(void*, char*, char*);
+
+struct DbInterface {
+    /* Basic */
+    open_func open;
+    close_func close;
+    delete_func delete;
+    fetch_func fetch;
+    try_store_func try_store;
+    store_func store;
+
+    /* Cursors */
+    create_cursor_func create_cursor;
+    cursor_first_func cursor_first;
+    cursor_next_func cursor_next;
+    cursor_key_func cursor_key;
+    cursor_value_func cursor_value;
+    destroy_cursor_func destroy_cursor;
+
+    /* Errors */
+    errno_func get_errno;
+    strerror_func strerror;
+};
+
+#endif /* DB_H__ */
