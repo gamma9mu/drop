947232f8651b6ef627e4c8505043f5c81298bba5
Brian Guthrie <baguthrie@sbcglobal.net>
2013-05-12 20:56:53 -0500

    Malloc TC DbInterface; don't return a local.

--- db_tcbdb.c
+++ db_tcbdb.c
@@ -12,7 +12,7 @@ static char *tcdb_cursor_value(void*, void*);
 static void  tcdb_destroy_cursor(void*);
 static void *tcdb_open(const char*);
 
-struct DbInterface get_interface(void);
+struct DbInterface *get_interface(void);
 
 static bool
 tcdb_close(void *db) {
@@ -64,22 +64,25 @@ tcdb_open(const char *file) {
     return db;
 }
 
-struct DbInterface
+struct DbInterface *
 get_interface() {
-    struct DbInterface dbint;
-    dbint.open = (open_func) tcdb_open;
-    dbint.close = (close_func) tcdb_close;
-    dbint.get_errno = (errno_func) &tcbdbecode;
-    dbint.strerror = (strerror_func) &tcbdberrmsg;
-    dbint.delete = (delete_func) &tcbdbout2;
-    dbint.fetch = (fetch_func) &tcbdbget2;
-    dbint.try_store = (try_store_func) &tcbdbputkeep2;
-    dbint.store = (store_func) &tcbdbput2;
-    dbint.create_cursor = (create_cursor_func) &tcdb_create_cursor;
-    dbint.destroy_cursor = (destroy_cursor_func) &tcdb_destroy_cursor;
-    dbint.cursor_first = (cursor_first_func) &tcdb_cursor_first;
-    dbint.cursor_next = (cursor_next_func) &tcdb_cursor_next;
-    dbint.cursor_key = (cursor_key_func) &tcdb_cursor_key;
-    dbint.cursor_value = (cursor_value_func) &tcdb_cursor_value;
+    struct DbInterface *dbint = malloc(sizeof(struct DbInterface));
+    if (dbint == NULL) {
+        return dbint;
+    }
+    dbint->open = (open_func) tcdb_open;
+    dbint->close = (close_func) tcdb_close;
+    dbint->get_errno = (errno_func) &tcbdbecode;
+    dbint->strerror = (strerror_func) &tcbdberrmsg;
+    dbint->delete = (delete_func) &tcbdbout2;
+    dbint->fetch = (fetch_func) &tcbdbget2;
+    dbint->try_store = (try_store_func) &tcbdbputkeep2;
+    dbint->store = (store_func) &tcbdbput2;
+    dbint->create_cursor = (create_cursor_func) &tcdb_create_cursor;
+    dbint->destroy_cursor = (destroy_cursor_func) &tcdb_destroy_cursor;
+    dbint->cursor_first = (cursor_first_func) &tcdb_cursor_first;
+    dbint->cursor_next = (cursor_next_func) &tcdb_cursor_next;
+    dbint->cursor_key = (cursor_key_func) &tcdb_cursor_key;
+    dbint->cursor_value = (cursor_value_func) &tcdb_cursor_value;
     return dbint;
 }
